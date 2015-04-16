922bcf98c7edda43bcbf7196e7ff0a0590b99b41
Hector Fabio Cadavid Rengifo <hcadavid@Hectors-MacBook-Pro.local>
2015-04-16 08:56:34 -0500

    first commit

--- src/main/java/edu/eci/cosw/services/ServicesFacade.java
+++ src/main/java/edu/eci/cosw/services/ServicesFacade.java
@@ -7,8 +7,9 @@ package edu.eci.cosw.services;
 
 import edu.eci.cosw.samples.model.Blueprint;
 import edu.eci.cosw.samples.model.Point;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,27 +19,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ServicesFacade {
    
-    private static final List<Blueprint> dummyBluePrints=new LinkedList<>();
+    private static final Map<String,Blueprint> dummyBluePrints=new LinkedHashMap<>();
     
     static{
-        dummyBluePrints.add(new Blueprint(new Point[]{new Point(0,0),new Point(10,10)}));
-        dummyBluePrints.add(new Blueprint(new Point[]{new Point(20,20),
+        dummyBluePrints.put("larecta", new Blueprint("larecta",new Point[]{new Point(0,0),new Point(10,10)}));
+        dummyBluePrints.put("rayon",new Blueprint("rayon",new Point[]{new Point(20,20),
                                                       new Point(30,30),
                                                       new Point(45,80)
         }));
-        dummyBluePrints.add(new Blueprint(new Point[]{new Point(3,44),
+        dummyBluePrints.put("poligono1",new Blueprint("poligono1",new Point[]{new Point(3,44),
                                                       new Point(66,34),
                                                       new Point(75,33),
                                                       new Point(100,100)
         }));    
     }
     
-    public void addNewBlueprint(Blueprint bp){
-        dummyBluePrints.add(bp);
+    public void addNewBlueprint(String name,Blueprint bp){
+        dummyBluePrints.put(name,bp);
     }
     
-    public List<Blueprint> getAllBlueprints(){
-        return dummyBluePrints;
+    public Set<String> getBlueprintNames(){
+        return dummyBluePrints.keySet();
+    }
+    
+    public Blueprint getBlueprintByName(String name){
+        return dummyBluePrints.get(name);
     }
     
 }
